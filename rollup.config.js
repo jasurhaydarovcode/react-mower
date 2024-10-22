3d7db2943bc1d1feef22f30dfeb130f79812bf10
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-10-22 14:30:40 +0500

    First Commit

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+export default [
+    {
+        input: 'src/index.tsx',
+        output: [
+            {
+                file: 'dist/index.js',
+                format: 'cjs',
+            },
+            {
+                file: 'dist/index.esm.js',
+                format: 'esm',
+            },
+        ],
+        external: ['react', 'react-dom', 'react-toastify'],
+        plugins: [
+            resolve(),
+            commonjs(),
+            typescript(),
+        ],
+    },
+    {
+        input: 'src/index.tsx',  // Deklaratsiya uchun asosiy fayl
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()],
+    },
+];
---
+++ src/index.tsx
@@ -0,0 +1,2 @@
+export { default as DevToolsBlocker } from './components/DevToolsBlocker';
+export { default as CursorProtect } from './components/CursorProtect';
